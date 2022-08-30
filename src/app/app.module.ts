e052fffc5ca598847f42bc563748f015c7e75749
hapi-csc <hayden.piper@ibm.com>
2022-08-30 14:24:32 -0500

    made it look pretty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LiveWeatherComponent } from './live-weather/live-weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LiveWeatherComponent
   ],
   imports: [
     BrowserModule
